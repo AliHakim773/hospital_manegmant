909f4b87de0b9d23acca94bae4dbd3b678c10374
AliHakim773 <alihakim7732@gmail.com>
2023-12-02 17:05:13 +0200

    added sign in logic

--- api/auth/sign-in.php
+++ api/auth/sign-in.php
@@ -15,3 +15,20 @@ $query->store_result();
 $num_rows = $query->num_rows; // to use num_rows we need to store results
 $query->bind_result($id, $role, $hashed_password); // taken from the query in prepare
 $query->fetch(); // to save the bind-results
+
+$response = [];
+if ($num_rows == 0) {
+    $response['status'] = 'user not found';
+    echo json_encode($response);
+} else {
+    if (password_verify($password, $hashed_password)) {
+        $response['status'] = 'logged in';
+        $response['user_id'] = $id;
+        $response['username'] = $username;
+        $response['role'] = $role;
+        echo json_encode($response);
+    } else {
+        $response['status'] = 'wrong credntails';
+        echo json_encode($response);
+    }
+}
