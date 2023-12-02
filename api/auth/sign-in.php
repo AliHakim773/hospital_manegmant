b2e8056f4bf62a0d3db99776909f2ff8ecdfd038
AliHakim773 <alihakim7732@gmail.com>
2023-12-02 17:03:47 +0200

    storing the results of the query

--- api/auth/sign-in.php
+++ api/auth/sign-in.php
@@ -10,3 +10,8 @@ $password = $_POST['password'];
 $query = $mysqli->prepare('select user_id, role, password from users where username=?');
 $query->bind_param('s', $username);
 $query->execute();
+
+$query->store_result();
+$num_rows = $query->num_rows; // to use num_rows we need to store results
+$query->bind_result($id, $role, $hashed_password); // taken from the query in prepare
+$query->fetch(); // to save the bind-results
