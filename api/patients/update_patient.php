91a3d23eba253da96ac9669b1ca72d81dca2418f
AliHakim773 <alihakim7732@gmail.com>
2023-12-02 20:15:32 +0200

    added condition to check if the user exist in update pateint

--- api/patients/update_patient.php
+++ api/patients/update_patient.php
@@ -28,10 +28,26 @@ try {
 } catch (Exception $e) {
     $response['status'] = 'fail';
     $response['msg'] = 'invalid token';
+    $response['error'] = $e;
     echo json_encode($response);
 }
 
 try {
+    //checking if the patient exists
+    $query = $mysqli->prepare('select role from users where user_id=?');
+    $query->bind_param('s', $user_id);
+    $query->execute();
+
+    $query->store_result();
+    $num_rows = $query->num_rows;
+    $query->bind_result($role);
+    $query->fetch();
+
+    if ($num_rows == 0)
+        die("user doen't exist");
+    if ($role != "patient")
+        die('not a patient');
+
     if ($data->role == 'admin') {
         $query =
             $mysqli->prepare('update information 
@@ -56,5 +72,6 @@ try {
 } catch (Exception $e) {
     $response['status'] = 'fail';
     $response['msg'] = 'error';
+    $response['error'] = $e;
     echo json_encode($response);
 }
