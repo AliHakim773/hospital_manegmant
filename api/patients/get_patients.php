ad08c137d93178be459b55b9230d5a32214a4264
AliHakim773 <alihakim7732@gmail.com>
2023-12-02 18:26:20 +0200

    added query to get patients with informations

--- api/patients/get_patients.php
+++ api/patients/get_patients.php
@@ -16,9 +16,16 @@ $response = [];
 try {
     $data = JWT::decode($token, new Key($secretKey, 'HS256'));
     if ($data->role == 'admin') {
+        $query = $mysqli->prepare('select patients.*, information.* from patients patients join users on patients.user_id = users.user_id join information on users.information_id = information.information_id');
+        $query->execute();
 
+        $array = $query->get_result();
 
-        echo 'admin';
+        while ($patients = $array->fetch_assoc()) {
+            $response[] = $patients;
+        }
+
+        echo json_encode($response);
     } else {
         $response['status'] = 'fail';
         $response['msg'] = 'access denied';
@@ -29,5 +36,5 @@ try {
 } catch (Exception $e) {
     $response['status'] = 'fail';
     $response['msg'] = 'invalid token';
-    echo $response;
+    echo json_encode($response);
 }
