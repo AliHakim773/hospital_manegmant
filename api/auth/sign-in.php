7f02169da3aa4e877451905b8641b013141c3740
AliHakim773 <alihakim7732@gmail.com>
2023-12-02 17:02:21 +0200

    getting the user with the same username from users

--- api/auth/sign-in.php
+++ api/auth/sign-in.php
@@ -5,3 +5,8 @@ include('../database/index.php');
 
 $username = $_POST['username'];
 $password = $_POST['password'];
+
+// getting the user with the same username from users
+$query = $mysqli->prepare('select user_id, role, password from users where username=?');
+$query->bind_param('s', $username);
+$query->execute();
