9746cdd935ce3f63de3ca785fbe86418c3d28bb9
AliHakim773 <alihakim7732@gmail.com>
2023-12-03 15:10:11 +0200

    get doctor by id

---
+++ api/doctors/get_doctor_by_id.php
@@ -0,0 +1,48 @@
+<?php
+
+header('Access-Controll-Allow-Origin:*');
+include('../database/index.php');
+include('../vendor/autoload.php');
+
+use Firebase\JWT\JWT;
+use \Firebase\JWT\Key;
+
+$allheaders = getallheaders();
+$token = $allheaders['Authorization'];
+$secretKey = 'secret';
+
+$response = [];
+try {
+    $data = JWT::decode($token, new Key($secretKey, 'HS256'));
+} catch (Exception $e) {
+    $response['status'] = 'fail';
+    $response['msg'] = 'invalid token';
+    echo json_encode($response);
+}
+
+$doctor_id = $_GET['doctor_id'];
+
+try {
+    if ($data->role == 'admin') {
+        $query = $mysqli->prepare('select * from doctors where doctor_id=?');
+        $query->bind_param('i', $doctor_id);
+        $query->execute();
+
+        $doctor = $query->get_result()->fetch_assoc();
+
+        if ($doctor == null) die('doctor not available');
+
+        $response['status'] = 'success';
+        $response['date'] = $doctor;
+
+        echo json_encode($response);
+    } else {
+        $response['status'] = 'fail';
+        $response['msg'] = 'access denied';
+        echo json_encode($response);
+    }
+} catch (Exception $e) {
+    $response['status'] = 'fail';
+    $response['msg'] = 'failed to delete doctor';
+    echo json_encode($response);
+}
