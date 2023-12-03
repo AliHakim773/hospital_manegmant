8552abf210c2518c8df32c805affd452eefdff1d
AliHakim773 <alihakim7732@gmail.com>
2023-12-03 15:34:13 +0200

    configred react router

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,19 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom"
 import "./styles/index.css"
+import Home from "./pages/Home"
+import Sginin from "./pages/Signin"
 
 function App() {
-    return <div className='app'></div>
+    return (
+        <div className='app'>
+            <BrowserRouter>
+                <Routes>
+                    <Route element={<Home />} path={"/"} />
+                    <Route element={<Sginin />} path={"/sign-in"} />
+                </Routes>
+            </BrowserRouter>
+        </div>
+    )
 }
 
 export default App
---
+++ frontend/src/pages/Signin/index.jsx
@@ -0,0 +1,7 @@
+import React from "react"
+
+function Sginin() {
+    return <div>Sign</div>
+}
+
+export default Sginin
