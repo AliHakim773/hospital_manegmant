7b62047a6ee1fd51e2077b9d1edadf141897ab3f
AliHakim773 <alihakim7732@gmail.com>
2023-12-03 11:53:03 +0200

    added try catch for the query

--- api/appointments/add_appointment.php
+++ api/appointments/add_appointment.php
@@ -27,4 +27,20 @@ $schedule_id = $_POST['schedule_id'];
 $room_id = $_POST['room_id'];
 $status = 'pending';
 
-echo $doctor_id, $room_id, $patient_id, $schedule_id, $status;
+try {
+    // added data to the users table
+    $query = $mysqli->prepare("insert into appointments(doctor_id,patient_id,schedule_id,room_id,status) 
+        values(?,?,?,?,{$status})");
+    $query->bind_param('iiii', $doctor_id, $patient_id, $schedule_id, $room_id);
+    $query->execute();
+
+
+
+    $response['status'] = 'success';
+    $response['msg'] = 'doctor added successfully';
+    echo json_encode($response);
+} catch (Exception $e) {
+    $response['status'] = 'fail';
+    $response['msg'] = 'failed to add appointment';
+    echo json_encode($response);
+}
