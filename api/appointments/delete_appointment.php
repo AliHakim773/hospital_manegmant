f0747ac9686ab6a1ed2317058c2b36a9a1f73bfb
AliHakim773 <alihakim7732@gmail.com>
2023-12-03 13:15:55 +0200

    delete an apointment with id

---
+++ api/appointments/delete_appointment.php
@@ -0,0 +1,52 @@
+<?php
+
+header('Access-Controll-Allow-Origin:*');
+include('../database/index.php');
+include('../vendor/autoload.php');
+
+use Firebase\JWT\JWT;
+use \Firebase\JWT\Key;
+
+$allheaders = getallheaders();
+$token = $allheaders['Authorization'];
+$secretKey = 'secret';
+
+$response = [];
+try {
+    $data = JWT::decode($token, new Key($secretKey, 'HS256'));
+} catch (Exception $e) {
+    $response['status'] = 'fail';
+    $response['msg'] = 'invalid token';
+    echo json_encode($response);
+}
+
+// getting data from url
+$appointment_id = $_POST['appointment_id'];
+
+
+try {
+    // cheching if the room is available
+    $query = $mysqli->prepare('select appointment_id from appointments where appointment_id=?');
+    $query->bind_param('i', $appointment_id);
+    $query->execute();
+
+    $query->store_result();
+    $num_rows = $query->num_rows;
+
+    if ($num_rows == 0) die('Appointment not available');
+
+    // added data to the users table
+    $query = $mysqli->prepare("delete from appointments where appointment_id=?");
+    $query->bind_param('i', $appointment_id);
+    $query->execute();
+
+
+
+    $response['status'] = 'success';
+    $response['msg'] = 'Appointment deleted';
+    echo json_encode($response);
+} catch (Exception $e) {
+    $response['status'] = 'fail';
+    $response['msg'] = 'failed to delete appointment';
+    echo json_encode($response);
+}
