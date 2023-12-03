250aa404207cda5ad3dd88f51a0002d86dd693dd
AliHakim773 <alihakim7732@gmail.com>
2023-12-03 15:17:15 +0200

    updated get doctor by id query

--- api/doctors/get_doctor_by_id.php
+++ api/doctors/get_doctor_by_id.php
@@ -24,7 +24,10 @@ $doctor_id = $_GET['doctor_id'];
 
 try {
     if ($data->role == 'admin') {
-        $query = $mysqli->prepare('select * from doctors where doctor_id=?');
+        $query = $mysqli->prepare('select users.username, users.role, doctors.*, information.* from users 
+                    join doctors on users.user_id = doctors.user_id 
+                    join information on users.user_id = information.user_id 
+                    where doctors.doctor_id = ?');
         $query->bind_param('i', $doctor_id);
         $query->execute();
 
