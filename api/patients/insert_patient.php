a12d20785dcd4a512088bdaa4361a31cc29639a5
AliHakim773 <alihakim7732@gmail.com>
2023-12-03 13:19:59 +0200

    added admin varification

--- api/patients/insert_patient.php
+++ api/patients/insert_patient.php
@@ -35,49 +35,55 @@ $email = $_POST['email'];
 $phone_number = $_POST['phone_number'];
 $medical_history = $_POST['medical_history'];
 
-//checking if the username exists
-$query = $mysqli->prepare('select username from users where username=?');
-$query->bind_param('s', $username);
-$query->execute();
-
-$query->store_result();
-$query->bind_result($name);
-$query->fetch();
 
 try {
-    if ($username == $name) {
-        $response['status'] = 'fail';
-        $response['msg'] = 'username already exists';
-        die('error: ' . json_encode($response));
+    if ($data->role == 'admin') {
+        //checking if the username exists
+        $query = $mysqli->prepare('select username from users where username=?');
+        $query->bind_param('s', $username);
+        $query->execute();
+
+        $query->store_result();
+        $query->bind_result($name);
+        $query->fetch();
+
+        if ($username == $name) {
+            $response['status'] = 'fail';
+            $response['msg'] = 'username already exists';
+            die('error: ' . json_encode($response));
+        } else {
+            // added data to the users table
+            $query =
+                $mysqli->prepare("insert into users(username,password,role) 
+            values(?,?,'patient')");
+            $query->bind_param('ss', $username, $hashed_password);
+            $query->execute();
+
+            // got the id of the data added
+            $user_id = $mysqli->insert_id;
+
+            // added data to information table
+            $query =
+                $mysqli->prepare('insert into information(user_id, first_name,last_name,date_of_birth,address,gender,email,phone_number) 
+            values(?,?,?,?,?,?,?,?)');
+
+            $query->bind_param('issssssi', $user_id, $first_name, $last_name, $date_of_birth, $address, $gender, $email, $phone_number);
+            $query->execute();
+
+            // added data to patient table
+            $query =
+                $mysqli->prepare('insert into patients(user_id,medical_history) 
+            values(?,?)');
+            $query->bind_param('is', $user_id, $medical_history);
+            $query->execute();
+
+            $response['status'] = 'success';
+            $response['msg'] = 'patient added successfully';
+            echo json_encode($response);
+        }
     } else {
-        // added data to the users table
-        $query =
-            $mysqli->prepare("insert into users(username,password,role) 
-        values(?,?,'patient')");
-        $query->bind_param('ss', $username, $hashed_password);
-        $query->execute();
-
-        // got the id of the data added
-        $user_id = $mysqli->insert_id;
-
-        // added data to information table
-        $query =
-            $mysqli->prepare('insert into information(user_id, first_name,last_name,date_of_birth,address,gender,email,phone_number) 
-        values(?,?,?,?,?,?,?,?)');
-
-        $query->bind_param('issssssi', $user_id, $first_name, $last_name, $date_of_birth, $address, $gender, $email, $phone_number);
-        $query->execute();
-
-        // added data to patient table
-        $query =
-            $mysqli->prepare('insert into patients(user_id,medical_history) 
-        values(?,?)');
-        $query->bind_param('is', $user_id, $medical_history);
-        $query->execute();
-
-
-        $response['status'] = 'success';
-        $response['msg'] = 'patient added successfully';
+        $response['status'] = 'fail';
+        $response['msg'] = 'access denied';
         echo json_encode($response);
     }
 } catch (Exception $e) {
