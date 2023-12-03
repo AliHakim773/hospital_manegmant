202e468c9803e5f5313e091bf164e38b2593e572
AliHakim773 <alihakim7732@gmail.com>
2023-12-03 15:02:43 +0200

    get appointment by id

---
+++ api/appointments/get_appointments_by_id.php
@@ -0,0 +1,48 @@
+<?php
+
+header('Access-Controll-Allow-Origin:*');
+include('../database/index.php');
+include('../vendor/autoload.php');
+
+use Firebase\JWT\JWT;
+use \Firebase\JWT\Key;
+
+$allheaders = getallheaders();
+$token = $allheaders['Authorization'];
+$secretKey = 'secret';
+
+$response = [];
+try {
+    $data = JWT::decode($token, new Key($secretKey, 'HS256'));
+} catch (Exception $e) {
+    $response['status'] = 'fail';
+    $response['msg'] = 'invalid token';
+    echo json_encode($response);
+}
+
+$appointment_id = $_GET['appointment_id'];
+
+try {
+    if ($data->role == 'admin') {
+        $query = $mysqli->prepare('select * from appointments where appointment_id=?');
+        $query->bind_param('i', $appointment_id);
+        $query->execute();
+
+        $appointment = $query->get_result()->fetch_assoc();
+
+        if ($appointment == null) die('appointment not available');
+
+        $response['status'] = 'success';
+        $response['date'] = $appointment;
+
+        echo json_encode($response);
+    } else {
+        $response['status'] = 'fail';
+        $response['msg'] = 'access denied';
+        echo json_encode($response);
+    }
+} catch (Exception $e) {
+    $response['status'] = 'fail';
+    $response['msg'] = 'failed to delete appointment';
+    echo json_encode($response);
+}
