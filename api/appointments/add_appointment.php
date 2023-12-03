f539024616e5261635344c00ad9dfcf1422cd77a
AliHakim773 <alihakim7732@gmail.com>
2023-12-03 11:45:25 +0200

    getting data from url

--- api/appointments/add_appointment.php
+++ api/appointments/add_appointment.php
@@ -19,3 +19,12 @@ try {
     $response['msg'] = 'invalid token';
     echo json_encode($response);
 }
+
+// getting data from url
+$doctor_id = $_POST['doctor_id'];
+$patient_id = $_POST['patient_id'];
+$schedule_id = $_POST['schedule_id'];
+$room_id = $_POST['room_id'];
+$status = 'pending';
+
+echo $doctor_id, $room_id, $patient_id, $schedule_id, $status;
