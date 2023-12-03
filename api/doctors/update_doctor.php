d4ac1bc14f925b8843444bfd9e8b66fcd36c62cc
AliHakim773 <alihakim7732@gmail.com>
2023-12-03 13:19:06 +0200

    added admin varification

--- api/doctors/update_doctor.php
+++ api/doctors/update_doctor.php
@@ -33,38 +33,44 @@ try {
 }
 
 try {
-    //checking if the doctor exists
-    $query = $mysqli->prepare('select role from users where user_id=?');
-    $query->bind_param('s', $user_id);
-    $query->execute();
-
-    $query->store_result();
-    $num_rows = $query->num_rows;
-    $query->bind_result($role);
-    $query->fetch();
-
-    if ($num_rows == 0)
-        die("user doen't exist");
-    if ($role != "doctor")
-        die('not a doctor');
-
     if ($data->role == 'admin') {
-        $query =
-            $mysqli->prepare('update information 
-        set first_name=?, last_name=?, date_of_birth=?, address=?, email=?, phone_number=? 
-        where user_id=?');
-        $query->bind_param('sssssii', $first_name, $last_name, $date_of_birth, $address, $email, $phone_number, $user_id);
-        $query->execute();
-        $query =
-            $mysqli->prepare('update doctors 
-                set specialization=?
-                where user_id=?');
-        $query->bind_param('si', $specialization, $user_id);
+        //checking if the doctor exists
+        $query = $mysqli->prepare('select role from users where user_id=?');
+        $query->bind_param('s', $user_id);
         $query->execute();
 
-        $response['status'] = 'success';
-        $response['msg'] = 'update successful';
-        echo json_encode($response);
+        $query->store_result();
+        $num_rows = $query->num_rows;
+        $query->bind_result($role);
+        $query->fetch();
+
+        if ($num_rows == 0)
+            die("user doen't exist");
+        if ($role != "doctor")
+            die('not a doctor');
+
+        if ($data->role == 'admin') {
+            $query =
+                $mysqli->prepare('update information 
+            set first_name=?, last_name=?, date_of_birth=?, address=?, email=?, phone_number=? 
+            where user_id=?');
+            $query->bind_param('sssssii', $first_name, $last_name, $date_of_birth, $address, $email, $phone_number, $user_id);
+            $query->execute();
+            $query =
+                $mysqli->prepare('update doctors 
+                    set specialization=?
+                    where user_id=?');
+            $query->bind_param('si', $specialization, $user_id);
+            $query->execute();
+
+            $response['status'] = 'success';
+            $response['msg'] = 'update successful';
+            echo json_encode($response);
+        } else {
+            $response['status'] = 'fail';
+            $response['msg'] = 'access denied';
+            echo json_encode($response);
+        }
     } else {
         $response['status'] = 'fail';
         $response['msg'] = 'access denied';
