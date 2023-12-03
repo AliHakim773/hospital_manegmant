fe8424032edf06027adfaf0561d9e335eac0a8fe
AliHakim773 <alihakim7732@gmail.com>
2023-12-03 11:39:50 +0200

    added add_sppointments with basic code

---
+++ api/appointments/add_appointment.php
@@ -0,0 +1,21 @@
+<?php
+
+header('Access-Controll-Allow-Origin:*');
+include('../database/index.php');
+include('../vendor/autoload.php');
+
+use Firebase\JWT\JWT;
+use \Firebase\JWT\Key;
+
+$allheaders = getallheaders();
+$token = $allheaders['Authorization'];
+$secretKey = 'secret';
+
+$response = [];
+try {
+    $data = JWT::decode($token, new Key($secretKey, 'HS256'));
+} catch (Exception $e) {
+    $response['status'] = 'fail';
+    $response['msg'] = 'invalid token';
+    echo json_encode($response);
+}
