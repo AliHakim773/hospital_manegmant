944b74bf2d5c1ea275fcf8ebdbc1f02c57580468
AliHakim773 <alihakim7732@gmail.com>
2023-12-04 14:29:18 +0200

    started the admin page

--- frontend/src/pages/admin/index.jsx
+++ frontend/src/pages/admin/index.jsx
@@ -1,12 +1,29 @@
-import React from 'react'
-import './styles.css'
+import React, { useEffect } from "react"
+import "./styles.css"
+import { requestData } from "../../core/axios"
 
 function Admin() {
-  return (
-    <div>
-      Admin
-    </div>
-  )
+    useEffect(() => {
+        const get = async () => {
+            const token = localStorage.getItem("token")
+            const headers = {
+                Authorization: `${token}`,
+            }
+            if (!token) {
+                console.error("Token not available")
+                return
+            }
+            const data = await requestData(
+                "/patients/get_patients.php",
+                "GET",
+                {},
+                headers
+            )
+            console.log(data)
+        }
+        get()
+    }, [])
+    return <div>Admin</div>
 }
 
 export default Admin
