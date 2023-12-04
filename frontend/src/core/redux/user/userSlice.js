cc892f0b447c0247f11a79259d53b6cf02a18c2b
AliHakim773 <alihakim7732@gmail.com>
2023-12-04 17:49:40 +0200

    exported clear user reducer

--- frontend/src/core/redux/user/userSlice.js
+++ frontend/src/core/redux/user/userSlice.js
@@ -28,7 +28,7 @@ export const userSlice = createSlice({
     },
 })
 
-export const { setUser } = userSlice.actions
+export const { setUser, clearUser } = userSlice.actions
 
 export default userSlice.reducer
 
