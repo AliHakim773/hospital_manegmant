4252a475143f54b5d7e4e9ec80d9a889bb4ad706
AliHakim773 <alihakim7732@gmail.com>
2023-12-04 17:48:19 +0200

    added header

--- frontend/src/components/Header/index.jsx
+++ frontend/src/components/Header/index.jsx
@@ -1,9 +1,35 @@
 import React from "react"
+import { Link, useNavigate } from "react-router-dom"
+
+import { useSelector } from "react-redux"
+
+import { extractUserSlice } from "../../core/redux/user/userSlice"
 
 import "./styles.css"
 
 const Header = () => {
-    return <header></header>
+    const userState = useSelector(extractUserSlice)
+    const navigate = useNavigate()
+
+    const handleLogout = () => {
+        localStorage.removeItem("token")
+        navigate("/")
+    }
+
+    return (
+        <header className='header d-flex'>
+            <div className='logo'>Hospital System</div>
+            <nav className='nav d-flex'>
+                <Link to='/admin'>Admin</Link>
+                <Link to='/patient'>Patient</Link>
+                <Link to='/doctor'>Doctor</Link>
+            </nav>
+            <div className='account d-flex flex-center flex-column'>
+                <div>{userState.username}</div>
+                <button onClick={handleLogout}>Logout</button>
+            </div>
+        </header>
+    )
 }
 
 export default Header
